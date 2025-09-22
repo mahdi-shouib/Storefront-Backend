959dbe822847c609d023c738ca6e17485043c7a0
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-22 21:05:06 +0300

    Add create method to products handler and fixed routes request methods

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { Product, ProductStore } from '../models/product';
+import { authorize } from '../middlewares/authorize';
 
 const store = new ProductStore();
 
@@ -13,9 +14,24 @@ const show = async (req: Request, res: Response) => {
 	res.json(product);
 };
 
+const create = async (req: Request, res: Response) => {
+	try {
+		const new_product: Omit<Product, 'id'> = {
+			name: req.body.name,
+			price: req.body.price,
+			category: req.body.category,
+		};
+		const product = await store.create(new_product as Product);
+		res.json(product);
+	} catch (err) {
+		res.status(400).send(String(err));
+	}
+};
+
 const products_routes = (app: express.Application) => {
-	app.use('/products', index);
-	app.use('/products/:id', show);
+	app.get('/products', index);
+	app.get('/products/:id', show);
+	app.post('/products', authorize, create);
 };
 
 export default products_routes;
