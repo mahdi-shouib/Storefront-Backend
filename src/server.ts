c2b65e4261eb906c36f6b7c1076f5ff5cf20aa24
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-22 21:19:38 +0300

    Add products routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import products_routes from './handlers/products';
 
 const app: express.Application = express();
 const port: number = 3000;
 
 app.use(express.json());
+
+products_routes(app);
 
 app.get('/', function (_req: Request, res: Response) {
 	res.send('Hello World!');
