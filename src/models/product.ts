d262d9edc19b7af92b8f3a651baabc84da65d55f
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-22 19:57:58 +0300

    Add index method to product model

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,11 +1,17 @@
-import db from "../db";
+import db from '../db';
 
 export type Product = {
-    id: number;
-    name: string;
-    price: number;
-}
+	id: number;
+	name: string;
+	price: number;
+};
 
 export class ProductStore {
-    
+	async index(): Promise<Product[]> {
+		const sql = 'SELECT * FROM products';
+		const conn = await db.connect();
+		const result = await conn.query(sql);
+		conn.release();
+		return result.rows;
+	}
 }
