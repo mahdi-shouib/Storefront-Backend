1123d586e690b7551aff448536cbc0409b7612f5
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-23 21:21:14 +0300

    Add create method

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import db from '../db';
+import bcrypt from 'bcrypt';
 
 export type User = {
 	id: number;
@@ -20,6 +21,19 @@ export class UserStore {
 		const sql = 'SELECT * FROM users WHERE id=($1)';
 		const conn = await db.connect();
 		const result = await conn.query(sql, [id]);
+		conn.release();
+		return result.rows[0];
+	}
+
+	async create(u: User): Promise<User> {
+		const sql =
+			'INSERT INTO users(firstname, lastname, password) VALUES($1, $2, $3) RETURNING *';
+		const hash = bcrypt.hashSync(
+			u.password + process.env.BCRYPT_SECRET,
+			process.env.SALT_ROUNDS!,
+		);
+		const conn = await db.connect();
+		const result = await conn.query(sql, [u.first_name, u.last_name, hash]);
 		conn.release();
 		return result.rows[0];
 	}
