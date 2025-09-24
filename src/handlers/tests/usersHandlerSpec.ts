1ecc52fa75807d4b3f40dd31051f2a482bddb2bb
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-24 17:03:40 +0300

    Add GET /users endpoint tests

--- src/handlers/tests/usersHandlerSpec.ts
+++ src/handlers/tests/usersHandlerSpec.ts
@@ -2,7 +2,42 @@ import supertest from 'supertest';
 import app from '../../server';
 import jwt from 'jsonwebtoken';
 import db from '../../db';
+import { User, UserStore } from '../../models/user';
 
 const request = supertest(app);
+const store = new UserStore();
 
-describe('User Endpoints Tests', () => {});
+describe('User Endpoints Tests', () => {
+	let test_token: string;
+	const test_user: Omit<User, 'id'> = {
+		firstname: 'test_firstname',
+		lastname: 'test_lastname',
+		password: 'test_password',
+	};
+
+	beforeAll(async () => {
+		const user = await store.create(test_user as User);
+		test_token = jwt.sign({ user: user }, process.env.TOKEN_SECRET!);
+	});
+
+	it('GET /users with valid token returns a list of all users', async () => {
+		const response = await request
+			.get('/users')
+			.set('Authorization', `Bearer ${test_token}`);
+		expect(response.ok).toBe(true);
+		expect(response.body.length).toBe(1);
+		expect(response.body[0].firstname).toBe('test_firstname');
+	});
+
+	it('GET /users with invalid token should fail', async () => {
+		const response = await request.get('/users');
+		expect(response.unauthorized).toBe(true);
+	});
+
+	afterAll(async () => {
+		const conn = await db.connect();
+		await conn.query('DELETE FROM users');
+		await conn.query('ALTER SEQUENCE users_id_seq RESTART WITH 1');
+		conn.release();
+	});
+});
