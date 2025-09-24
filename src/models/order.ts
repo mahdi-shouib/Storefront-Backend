b466f9a4fe7ac8d8ce08cd65b37f292ce14185f5
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-24 19:25:36 +0300

    Add add product method

--- src/models/order.ts
+++ src/models/order.ts
@@ -6,4 +6,17 @@ export type Order = {
 	status: string;
 };
 
-export class OrderStore {}
+export class OrderStore {
+	async addProduct(
+		order_id: string,
+		product_id: string,
+		quantity: number,
+	): Promise<Order> {
+		const sql =
+			'INSERT INTO order_products(order_id, product_id, quantity) VALUES($1, $2, $3) RETURNING *';
+		const conn = await db.connect();
+		const result = await conn.query(sql, [order_id, product_id, quantity]);
+		conn.release();
+		return result.rows[0];
+	}
+}
