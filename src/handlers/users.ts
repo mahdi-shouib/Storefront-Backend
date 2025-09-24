afa61e9cb22b7a2bb3d9d980a3f282e616d7f846
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-24 15:22:18 +0300

    Add index and show methods to users handler

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -4,6 +4,24 @@ import authorize from '../middlewares/authorize';
 
 const store = new UserStore();
 
-const users_routes = (app: express.Application) => {};
+const index = async (_req: Request, res: Response) => {
+	const users = await store.index();
+	res.json(users);
+};
+
+const show = async (req: Request, res: Response) => {
+	const user = await store.show(req.params.id);
+	if (!user) {
+		res.status(404).send('User Not Found!');
+		return;
+	}
+	res.json(user);
+};
+
+const users_routes = (app: express.Application) => {
+	app.use(authorize);
+	app.get('/users', index);
+	app.get('/users/:id', show);
+};
 
 export default users_routes;
