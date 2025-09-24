1f30ddd8af05220fed7fa5e1a1599ce18203371b
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-24 14:59:39 +0300

    Add user endpoints base tests

---
+++ src/handlers/tests/usersHandlerSpec.ts
@@ -0,0 +1,8 @@
+import supertest from 'supertest';
+import app from '../../server';
+import jwt from 'jsonwebtoken';
+import db from '../../db';
+
+const request = supertest(app);
+
+describe('User Endpoints Tests', () => {});
