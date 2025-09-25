57df7ac675e181ca55cebf748b2494f0bb8fabd7
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-25 15:37:44 +0300

    Add POST /orders endpoint

--- src/handlers/orders.ts
+++ src/handlers/orders.ts
@@ -4,8 +4,26 @@ import authorize from '../middlewares/authorize';
 
 const store = new OrderStore();
 
+const create = async (req: Request, res: Response) => {
+	const { user_id, status } = req.body;
+
+	if (!user_id || !status) {
+		res.status(400).send('Invalid Request!');
+		return;
+	}
+
+	const new_order: Omit<Order, 'id'> = {
+		user_id,
+		status,
+	};
+
+	const order = await store.create(new_order as Order);
+	res.json(order);
+};
+
 const orders_routes = (app: express.Application) => {
 	app.use(authorize);
+	app.post('/orders', create);
 };
 
 export default orders_routes;
