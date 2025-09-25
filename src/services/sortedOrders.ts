1990868bd1ab703ac2446d39c20ea3f7fd201629
mahdi-shouib <mahdiomar402@gmail.com>
2025-09-25 16:17:18 +0300

    Add order by status service endpoint route

--- src/services/sortedOrders.ts
+++ src/services/sortedOrders.ts
@@ -4,8 +4,21 @@ import { OrderStore } from '../models/order';
 
 const store = new OrderStore();
 
+const orderByStatus = async (req: Request, res: Response) => {
+	const { id, status } = req.params;
+
+	if (!id || (status !== 'open' && status !== 'complete')) {
+		res.status(400).send('Invalid Request!');
+		return;
+	}
+
+	const orders = await store.orderByStatus(id, status);
+	res.json(orders);
+};
+
 const sorted_orders_routes = (app: express.Application) => {
 	app.use(authorize);
+	app.get('/users/:id/orders/:status', orderByStatus);
 };
 
 export default sorted_orders_routes;
